007a4cd21e2832d68f9f1df9115b4a09b97fa901
Pablo Quiroga <quirogaroot@outlook.com>
2022-10-02 14:20:06 -0300

    final

---
+++ js/events.js
@@ -0,0 +1,55 @@
+import {
+    buttonPlay,
+    buttonPause,
+    buttonSet,
+    buttonStop,
+    buttonSoundOn,
+    buttonSoundOff
+} from "./elements.js";
+
+export default function ({ controls, timer, sound }) {
+    
+    buttonPlay.addEventListener('click', function() {    
+        controls.play()
+        timer.countDown()
+        sound.pressButton()
+    })
+
+    buttonPause.addEventListener('click', function(){
+        controls.pause()
+        timer.hold()
+        sound.pressButton()
+    })
+
+    buttonStop.addEventListener('click', function() {
+        controls.reset()
+        timer.reset()
+        sound.pressButton()
+    })
+
+    buttonSoundOn.addEventListener('click', function(){
+        buttonSoundOn.classList.add('hide');
+        buttonSoundOff.classList.remove('hide');
+        sound.bgAudio.play()
+    })
+
+    buttonSoundOff.addEventListener('click', function(){
+        buttonSoundOff.classList.add('hide');
+        buttonSoundOn.classList.remove('hide');
+        sound.bgAudio.pause()
+    })
+
+    buttonSet.addEventListener('click', function() {
+        let newMinutes = controls.getMinutes()
+
+        if(!newMinutes) {
+            timer.reset()
+            return
+        }
+
+        timer.updateDisplay(newMinutes, 0)
+        timer.updateMinutes(newMinutes)
+
+    })
+
+}
